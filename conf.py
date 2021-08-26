5d20eff6d3082567c5d904dc4aefac795d737160
Gustavo Orair <guru_gho@hotmail.com>
2021-08-26 16:46:29 -0300

    Atualizando background no conf.py

--- conf.py
+++ conf.py
@@ -77,6 +77,25 @@ html_favicon = u'http://www.gov.br/governodigital/++theme++padrao_govbr/favicons
 
 html_logo = u'_imagens/logo_conecta.png'
 
+html_theme_options = {
+#    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+#    'analytics_anonymize_ip': False,
+#    'logo_only': False,
+    'display_version': False,
+#    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white'
+
+    # Toc options
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
+#    'includehidden': True,
+#    'titles_only': False
+}
+
+
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
