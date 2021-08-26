415d0d9d999a4eec135a14fb8ead49afdab9e14a
Gustavo Orair <guru_gho@hotmail.com>
2021-08-25 21:49:54 -0300

    Atualizando configurações do sphinx

--- conf.py
+++ conf.py
@@ -1,3 +1,9 @@
+# Project name
+project = 'Gerenciados de APIs do Conecta'
+
+# Language
+language = pt_BR
+
 # Add the extension
 extensions = [
    'sphinx.ext.autosectionlabel',
@@ -8,3 +14,16 @@ autosectionlabel_prefix_document = True
 
 # Enable numref
 numfig = True
+
+# Show copyright
+html_show_copyright = False
+
+# Show "Created using Sphinx"
+html_show_sphinx = False
+
+# Show links to the reST sources will be added to the sidebar
+html_show_sourcelink = False
+
+# Language to be used for generating the HTML full-text search index.
+html_search_language = 'pt'
+
